182e6f4567a9b216b733d3d9c5dd7f2873c91249
Matthew Holder <sixxgate@hotmail.com>
2023-04-09 12:35:15 -0500

    Bug demo

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <VApp>
+    <VMain>
+      <HelloWorld />
+    </VMain>
+  </VApp>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <VContainer class="fill-height">
+    <VResponsive class="d-flex align-center text-center fill-height">
+      <VSelect v-model="item" label="Item" :items="items" placeholder="required"/>
+    </VResponsive>
+  </VContainer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { readonly } from 'vue';
+
+const items = readonly([
+  { title: 'Item one', value: 'i1' },
+  { title: 'Item one', value: 'i1' },
+  { title: 'Item one', value: 'i1' }
+])
+
+type Item = typeof items[number]
+
+const item = ref<Item>()
+</script>
